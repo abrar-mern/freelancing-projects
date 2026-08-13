b1a55779249f3ab01726b2c910452815ef1cfabe
Abrar Babar Khan <mrabrarbabarkhan@gmail.com>
2026-08-13 17:30:42 +0530

    Keep only Murphy project

--- Project Automation/server/routes/generateroute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import {generateResponse} from "../controllers/generateController.js"
-
-const router = express.Router();
-
-// Route to handle response generation
-router.post('/generate', generateResponse);
-
-export default router;
